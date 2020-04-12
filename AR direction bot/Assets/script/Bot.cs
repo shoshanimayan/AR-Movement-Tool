53761314a2384876227dfe1bd48ee79ffd0fab30
Mayan <mshoshan@uci.edu>
2020-04-12 02:33:48 -0700

    added intractable AR ui, path following, and ar movement

---
+++ AR direction bot/Assets/script/Bot.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bot : MonoBehaviour
+{
+    public GameObject sign;
+    private bool on = false;
+    private GameObject[] GoTo;
+    int count = 0;
+    int total = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (on) {
+
+            if (count < total)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, GoTo[count].transform.position, 1f * Time.deltaTime);
+                if (Vector3.Distance(transform.position, GoTo[count].transform.position) <0.001f)
+                {
+                    Destroy(GoTo[count]);
+                    count++;
+                }
+            }
+            else {
+                foreach (GameObject spot in spawn.spots) { Destroy(spot); }
+                on = false;
+                spawn.spots = new List<GameObject>();
+                count = 0;
+            }
+
+
+        } 
+    }
+
+    public void begin() {
+        total= spawn.spots.ToArray().Length;
+        on = true;
+        GoTo = spawn.spots.ToArray();
+        
+
+    }
+}
--- AR direction bot/Assets/script/spawn.cs
+++ AR direction bot/Assets/script/spawn.cs
@@ -8,7 +8,9 @@ public class spawn : MonoBehaviour
 {
     public GameObject prefab;
     public GameObject prefab2;
-
+    public static List<GameObject> spots;
+    private  GameObject bot;
+    private GameObject sign;
     private ARRaycastManager aRRaycastManager;
     private Vector2 touchPosition;
     private bool spawned = false;
@@ -16,6 +18,7 @@ public class spawn : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
+        spots = new List<GameObject>();
         aRRaycastManager = GetComponent<ARRaycastManager>();
     }
 
@@ -23,6 +26,10 @@ public class spawn : MonoBehaviour
     {
         if (Input.touchCount > 0)
         {
+            if (Input.GetTouch(0).phase != TouchPhase.Began) {
+                touchPosition = default;
+                return false;
+            }
             touchPosition = Input.GetTouch(0).position;
             return true;
         }
@@ -33,8 +40,10 @@ public class spawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (!spawned)
         {
+
             if (!TryGetPosition(out Vector2 touchPosition))
             {
                 return;
@@ -42,16 +51,63 @@ public class spawn : MonoBehaviour
             if (aRRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon) && !spawned)
             {
                 var hitPose = hits[0].pose;
-                Instantiate(prefab, hitPose.position, hitPose.rotation);
+                bot = Instantiate(prefab, hitPose.position, hitPose.rotation);
+                sign = bot.transform.GetChild(1).gameObject;
                 spawned = true;
 
             }
         }
         else {
+            if (!TryGetPosition(out Vector2 touchPosition))
+            {
+                return;
+            }
+            Debug.Log("markers");
 
+            Ray ray = Camera.main.ScreenPointToRay(touchPosition);
+            RaycastHit hitObject;
+            //if (spots.Count < 6) {
+                if (Physics.Raycast(ray, out hitObject))
+                {
+                    GameObject placementObject = hitObject.transform.gameObject;
+
+                    if (placementObject == bot) { Debug.Log("bot"); return; }
+                    else if (placementObject == bot.GetComponent<Bot>().sign) { Debug.Log("sign"); Begin(); }
+                    else
+                    {
+                        if (aRRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon) && spawned)
+                        {
+                            var hitPose = hits[0].pose;
+                            spots.Add(Instantiate(prefab2, hitPose.position, hitPose.rotation));
+                            spawned = true;
+
+                        }
+
+                    }
+                    }
+                else
+                {
+                        if (aRRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon) && spawned)
+                        {
+                            var hitPose = hits[0].pose;
+                            spots.Add(Instantiate(prefab2, hitPose.position, hitPose.rotation));
+                            spawned = true;
+
+                        }
+                }
+
+            //}
+            //else { Begin(); }
 
         }
 
 
     }
+
+    public void Begin() {
+
+        bot.GetComponent<Bot>().begin();
+
+    }
+
 }
